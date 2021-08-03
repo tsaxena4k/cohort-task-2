5f5643b89c1816770650d91bd5712965ac6dafdc
Tushar saxena <tsaxena3k@gmail.com>
2021-08-03 21:19:21 +0530

    commit 6

--- src/component/BlogContent.jsx
+++ src/component/BlogContent.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
-import cardImage from '../images/BG1.jpg'
+import React, { useState, useEffect } from 'react';
+import { AiFillHeart, AiOutlineComment } from 'react-icons/ai';
 
 function BlogContent() {
-    return (
-        <div className="blog-container">
-            <div className="container pt-5 pb-5">
-                <div className='row'>
-                    <div className="col-sm-12">
-                        <div class="card mb-3">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src={cardImage} class="img-fluid rounded-start" alt="..." />
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                    </div>
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch('https://dev.to/api/articles?username=tsaxena4k')
+            .then((res) => res.json())
+            .then((json) => setData(json))
+    }, []);
+
+    function blogCard(blog) {
+        return (
+            <div className="col-sm-12">
+                <div class="card mb-3 shadow">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src={blog.cover_image} class="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <a href={blog.url} target="_blank" className="blog-url"><h5 class="card-title">{blog.title}</h5></a>
+                                <p class="card-text">{blog.description}</p>
+                                <div className="d-flex public-reaction">
+                                    <p style={{marginRight:'10px'}}><AiFillHeart/> {blog.public_reactions_count}</p>
+                                    <p><AiOutlineComment /> {blog.comments_count}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="blog-container">
+            <div className="container pt-5 pb-5">
+                <div className='row'>
+                    {
+                        (Object.keys(data).length === 0 && data.constructor === Object) ?
+                            null :
+                            data.map((value) => {
+                                return blogCard(value);
+                            })
+                    }
                 </div>
             </div>
         </div>
